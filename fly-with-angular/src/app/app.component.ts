2034475c5153678ef7aa1523e11243315de28e5d
Rajdeep Mandrekar <rajdeepmandrekar26@gmail.com>
2017-12-08 13:45:08 +0530

    * First cut of the flights list UI

--- fly-with-angular/src/app/app.component.ts
+++ fly-with-angular/src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Trip } from '../app/models/trip';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Flights of a Lifetime';
+
+  trip: Trip = {
+    name: 'Super Flights to Mars',
+    price: 18200,
+    duration: '2 Earth Years',
+    description: 'Race through the Solar System to reach the Red planet. This flight will take 11 Earth Years for each side of the journey.',
+    image_url: 'https://angular.io/assets/images/logos/angular/angular.png'
+    // image_url: 'https://vignette.wikia.nocookie.net/vsbattles/images/e/e9/Mars_Planet.png/revision/latest?cb=20151113063011'
+  };
+
+  trips: Array<Trip> = [this.trip, this.trip, this.trip];
+
 }
---
+++ fly-with-angular/src/app/models/trip.ts
@@ -0,0 +1,7 @@
+export interface Trip {
+  name: string;
+  price: number;
+  duration: string;
+  description: string;
+  image_url: string;
+}
