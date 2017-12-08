369b41c95feba3a4bf92647ace847d4aaeb4c881
Rajdeep Mandrekar <rajdeepmandrekar26@gmail.com>
2017-12-09 01:20:56 +0530

    * Using ngFor and ngIf

--- fly-with-angular/src/app/components/trips-list/trips-list.component.ts
+++ fly-with-angular/src/app/components/trips-list/trips-list.component.ts
@@ -10,14 +10,29 @@ export class TripsListComponent implements OnInit {
 
   title = 'Flights of a Lifetime';
 
-  trip: Trip = {
-    name: 'Super Flights to Mars',
-    price: 18200,
-    duration: '2 Earth Years',
-    description: 'Race through the Solar System to reach the Red planet. This flight will take 11 Earth Years for each side of the journey.',
-    image_url: 'https://angular.io/assets/images/logos/angular/angular.png'
-    // image_url: 'https://vignette.wikia.nocookie.net/vsbattles/images/e/e9/Mars_Planet.png/revision/latest?cb=20151113063011'
-  };
+  trips: Array<Trip> = [
+    {
+      name: 'Super Flights to Mars',
+      price: 18200,
+      duration: '2 Earth Years',
+      description: 'Race through the Solar System to reach the Red planet. This flight will take 11 Earth Years for each side of the journey.',
+      image_url: 'https://angular.io/assets/images/logos/angular/angular.png'
+    },
+    {
+      name: 'Super Flights to Mars',
+      price: 18200,
+      duration: '2 Earth Years',
+      description: 'Race through the Solar System to reach the Red planet. This flight will take 11 Earth Years for each side of the journey.',
+      image_url: 'https://angular.io/assets/images/logos/angular/angular.png'
+    },
+    {
+      name: 'Super Flights to Mars',
+      price: 18200,
+      duration: '2 Earth Years',
+      description: 'Race through the Solar System to reach the Red planet. This flight will take 11 Earth Years for each side of the journey.',
+      image_url: 'https://angular.io/assets/images/logos/angular/angular.png'
+    }
+  ];
 
   constructor() { }
 
